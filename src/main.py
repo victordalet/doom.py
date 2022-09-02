561a5b75ab663c9ae7fa6ff477ba6321e96475d6
victordalet <victordalet@protonmail.com>
2022-09-02 14:24:10 +0200

    sprites

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ from map import *
 from player import * 
 from raycasting import *
 from object_rendered import *
+from sprite_object import * 
+from object_handler import * 
 
 class Game:
 	def __init__(self):
@@ -20,10 +22,12 @@ class Game:
 		self.player = Player(self)
 		self.object_rendered = ObjectRendered(self)
 		self.raycasting = Raycasting(self)
+		self.object_handler = ObjectHandler(self)
 
 	def update(self):
 		self.player.update()
 		self.raycasting.update()
+		self.object_handler.update()
 		pygame.display.flip()
 		self.delta_time =  self.clock.tick(FPS)
 		pygame.display.set_caption(f'{self.clock.get_fps() : .1f}')
